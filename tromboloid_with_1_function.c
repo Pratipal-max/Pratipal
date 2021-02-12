dfbe2736b6cad13cf0f7f18c303f198e624c942c
Pratipal-max <72759339+Pratipal-max@users.noreply.github.com>
2021-02-12 19:59:33 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,1 +1,11 @@
 //Write a program to find the volume of a tromboloid using one function
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+float h,d,b,voltromboloid;
+printf("enter 3 num");
+scanf("%f%f%f",&h,&d,&b);
+voltromboloid=(1.0)/(3.0)*((h*d*b)+(d/b));
+printf("voltromboloid = %f",voltromboloid);
+}
