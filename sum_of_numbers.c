fff4d4b16f198f098600ce0c38468361f36adbd4
Pratipal-max <72759339+Pratipal-max@users.noreply.github.com>
2021-02-17 19:04:54 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,32 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include<stdio.h>
+int input()
+{
+    int n;
+    printf("enter the value of n: \n");
+    scanf("%d",&n);
+    return n;
+}
+int calc(int n)
+{
+    int temp,add;
+    for(int i = 1; i<=n; i++ )
+        {
+        printf("%d enter the number: ",i);
+        scanf("%d",&temp);
+        add += temp;
+        }
+    return add;
+}
+void display(int sum)
+{
+    printf("sum of n number: %d ",sum);
+}
+int main()
+{
+    int sum,n;
+    n = input();
+    sum = calc(n);
+    display(sum);
+    return 0;
+}
